2b0fbe852b48f593b249382cf672250abcce6075
neha <neha309shar@gmail.com>
2022-06-09 21:44:10 +0530

    new change

---
+++ Dartbasics/Lesson-3/1-Functions.dart
@@ -0,0 +1,30 @@
+// Functions are of Two types
+// 1. System Defined Function
+// eg: main(), print(), write()
+// 2. User Defined | Custom Function
+
+// Function having two Parts
+// 1. Function Defination: What are tasks performed by the Function
+// Preferlly we put function defination to be outside of main()
+// 2. Function Calling: Call the Function that is already defined
+main(){
+  PrintSomething();
+  PrintSomething();
+  PrintSomething();
+
+}
+PrintSomething(){
+  print("this is user function");
+  print("you can call this function anywhere,anytime inside main fuction");
+  print(" ------------- ");
+}
+
+//there are various variant of a fuction
+//1- function without input (no parameter) and without return
+// 2. Function with no Input and with Return 
+// 3. Function with some Input and with Return
+
+// Category of Function in New Programming Language
+// 1. Named Function
+// 2. Expression Function
+// 3. Anonymous Function
---
+++ Dartbasics/Lesson-3/2-FunctionwithReturn.dart
@@ -0,0 +1,11 @@
+// 2. Function with no Input and with Return statement
+main(){
+  String Result =PrintSomething();
+  print("Returned Result: $Result");
+  print(PrintSomething());
+  print(PrintSomething());
+
+}
+PrintSomething(){
+  return "this is a return message";
+}
